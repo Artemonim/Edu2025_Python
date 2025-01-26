c47ae98e22b117ebb59bac81ca769281e3677839
Artemonim <Artemonim@yandex.ru>
2025-01-27 02:57:52 +0300

    Create 2.4.3.cube.py

---
+++ src/course_for_beginners/chapter_2/2.4.3.cube.py
@@ -0,0 +1,11 @@
+"""https://stepik.org/course/58852
+https://stepik.org/lesson/265079/step/8
+"""
+
+a = int(input())
+
+volume = a * a * a
+total_surface_area = 6 * a * a
+
+print(f"Объем = {volume}")
+print(f"Площадь полной поверхности = {total_surface_area}")
