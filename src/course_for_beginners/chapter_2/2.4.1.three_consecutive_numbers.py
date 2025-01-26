b72d3cf248b1eb0cdb7d8cf36de9c5f1a9f659c9
Artemonim <Artemonim@yandex.ru>
2025-01-27 02:50:40 +0300

    Create 2.4.1.three_consecutive_numbers.py

---
+++ src/course_for_beginners/chapter_2/2.4.1.three_consecutive_numbers.py
@@ -0,0 +1,7 @@
+"""https://stepik.org/course/58852
+https://stepik.org/lesson/265079/step/6
+"""
+orginal_number = int(input())
+
+print(orginal_number, orginal_number + 1, orginal_number + 2, sep='\n')
+
