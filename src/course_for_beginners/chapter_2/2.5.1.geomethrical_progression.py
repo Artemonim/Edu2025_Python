06688b057065b20794c292358a02aa32cd28f44e
Artemonim <Artemonim@yandex.ru>
2025-01-27 23:39:36 +0300

    Create 2.5.1.geomethrical_progression.py

---
+++ src/course_for_beginners/chapter_2/2.5.1.geomethrical_progression.py
@@ -0,0 +1,8 @@
+"""https://stepik.org/course/58852 - course for beginners
+https://stepik.org/lesson/284816/step/13
+"""
+b, q, n = int(input()), int(input()), int(input())
+
+desired_value = b * q ** (n - 1)
+
+print(desired_value)
