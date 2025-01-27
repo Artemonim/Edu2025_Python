843f0cb31b7a483bfb5a1e6006668b1eec308ae1
Artemonim <Artemonim@yandex.ru>
2025-01-27 19:57:34 +0300

    Create 2.4.7.arithmetic_operations.py

---
+++ src/course_for_beginners/chapter_2/2.4.7.arithmetic_operations.py
@@ -0,0 +1,8 @@
+"""https://stepik.org/course/58852
+https://stepik.org/lesson/265079/step/12
+"""
+a, b = int(input()), int(input())
+
+print(f'{a} + {b} = {a + b}')
+print(f'{a} - {b} = {a - b}')
+print(f'{a} * {b} = {a * b}')
