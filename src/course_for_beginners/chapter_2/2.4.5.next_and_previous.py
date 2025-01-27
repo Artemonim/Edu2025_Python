ad95c2434112541b6c3cd5961d2fa712532e7454
Artemonim <Artemonim@yandex.ru>
2025-01-27 17:02:19 +0300

    Create 2.4.5.next_and_previous.py

---
+++ src/course_for_beginners/chapter_2/2.4.5.next_and_previous.py
@@ -0,0 +1,10 @@
+"""https://stepik.org/course/58852
+https://stepik.org/lesson/265079/step/10
+"""
+
+start_number = int(input())
+previous_number = start_number - 1
+next_number = start_number + 1
+
+print(f'Следующее за числом {start_number} число: {next_number}')
+print(f'Для числа {start_number} предыдущее число: {previous_number}')
