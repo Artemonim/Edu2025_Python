b70936b9094d7a46007018b17d09441dfb8bac6f
Artemonim <Artemonim@yandex.ru>
2025-01-28 02:15:34 +0300

    Create 2.5.4.inevitability_itself.py

---
+++ src/course_for_beginners/chapter_2/2.5.4.inevitability_itself.py
@@ -0,0 +1,8 @@
+"""https://stepik.org/course/58852 - course for beginners
+https://stepik.org/lesson/284816/step/16
+"""
+universe_population = int(input())
+
+total_population = universe_population // 2 + universe_population % 2
+
+print(total_population)
