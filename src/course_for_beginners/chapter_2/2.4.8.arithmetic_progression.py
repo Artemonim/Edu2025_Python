4ec7cff3d14957593d285db4f4dca4472c1667cb
Artemonim <Artemonim@yandex.ru>
2025-01-27 20:01:30 +0300

    Create 2.4.8.arithmetic_progression.py

---
+++ src/course_for_beginners/chapter_2/2.4.8.arithmetic_progression.py
@@ -0,0 +1,8 @@
+"""https://stepik.org/course/58852
+https://stepik.org/lesson/265079/step/13
+"""
+a, d, n = int(input()), int(input()), int(input())
+
+desired_value = a + (n - 1) * d
+
+print(desired_value)
